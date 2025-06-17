e2b799551bfba33619a7b530801e811e026270d8
Eddie Zhuang <zhuang.eddie@gmail.com>
2025-06-17 13:35:54 -0400

    feat: add resizable panel components and dependencies
    - Introduced `ResizablePanelGroup`, `ResizablePanel`, and `ResizableHandle` components in `components/ui/resizable.tsx` for creating resizable panels.
    - Added dependencies:
      - `@neondatabase/serverless` version 1.0.1
      - `@radix-ui/react-navigation-menu` version 1.2.13
      - `monaco-editor` version 0.52.2
      - `react-resizable-panels` version 3.0.3

---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,15 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+export default function Home() {
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel>One</ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel>Two</ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
