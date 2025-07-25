a7a65025f7168c93be5f73931f8a36c451204f1a
Eddie Zhuang <zhuang.eddie@gmail.com>
2025-07-25 17:37:22 -0400

    Refactor SiteHeader component for improved layout and styling
    - Adjusted the flex properties and max-width of the SiteHeader for better alignment and responsiveness.
    - Streamlined the structure of the NavigationMenu and its items for enhanced clarity and maintainability.

--- components/marketing/site-header.tsx
+++ components/marketing/site-header.tsx
@@ -11,36 +11,34 @@ import { MobileNav } from "@/components/marketing/mobile-nav";
 
 export function SiteHeader() {
   return (
-    <NavigationMenu className="flex-0 sticky top-0 max-w-full bg-background">
-      <div className="w-full 3xl:fixed:px-0 px-6">
-        <div className="3xl:fixed:container flex h-14 items-center justify-between gap-2">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" className="flex flex-row gap-2">
-                <Image src="/typeset.svg" width={24} height={24} alt="Logo" />
-                <span className="text-base font-semibold">Typeset</span>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
+    <NavigationMenu className="flex justify-center flex-0 sticky top-0 max-w-full bg-background">
+      <div className="flex h-14 items-center justify-between gap-2 w-full box-content max-w-[1248px] px-6">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" className="flex flex-row gap-2">
+              <Image src="/typeset.svg" width={24} height={24} alt="Logo" />
+              <span className="text-base font-semibold">Typeset</span>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
 
-          <NavigationMenuList className="gap-2">
-            <NavigationMenuItem className="hidden lg:block">
-              <SignInButton>
-                <Button variant="ghost" size="sm">
-                  Log In
-                </Button>
-              </SignInButton>
-            </NavigationMenuItem>
-            <NavigationMenuItem className="hidden lg:block">
-              <SignUpButton>
-                <Button size="sm">Sign Up</Button>
-              </SignUpButton>
-            </NavigationMenuItem>
-            <NavigationMenuItem className="lg:hidden">
-              <MobileNav />
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </div>
+        <NavigationMenuList className="gap-2">
+          <NavigationMenuItem className="hidden lg:block">
+            <SignInButton>
+              <Button variant="ghost" size="sm">
+                Log In
+              </Button>
+            </SignInButton>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="hidden lg:block">
+            <SignUpButton>
+              <Button size="sm">Sign Up</Button>
+            </SignUpButton>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="lg:hidden">
+            <MobileNav />
+          </NavigationMenuItem>
+        </NavigationMenuList>
       </div>
     </NavigationMenu>
   );
